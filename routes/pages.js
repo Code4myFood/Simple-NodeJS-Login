d8c96278fcdcec9b8465e704184b82285e5bd2e9
Code4myFood <66778764+Code4myFood@users.noreply.github.com>
2020-08-07 04:33:00 +0800

    Add files via upload

---
+++ routes/pages.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', function(req, res){
+    res.render('index');
+});
+
+router.get('/register', function(req, res){
+    res.render('register');
+});
+
+router.get('/login', function(req, res){
+    res.render('login');
+});
+
+module.exports = router;
